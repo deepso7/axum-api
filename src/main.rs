c0f541dc8bee201c2e7e85ecc0c5eeafb80d552c
deepso <deeps.o7o8.y@gmail.com>
2024-08-10 01:52:00 +0530

    setup hellow router

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+use axum::{response::Html, routing::get, Router};
+use tokio::net::TcpListener;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello <strong>World!!!</strong>") }),
+    );
+
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    println!(
+        "->> LISTENING on http://{:?}\n",
+        listener.local_addr().unwrap()
+    );
+
+    axum::serve(listener, routes_hello.into_make_service())
+        .await
+        .unwrap();
 }
