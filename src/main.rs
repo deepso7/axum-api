b42036968b2abbec095c9ab80ac5f9bec6eec303
deepso <deeps.o7o8.y@gmail.com>
2024-08-10 02:13:02 +0530

    add path params route and structure code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    extract::{Path, Query},
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use serde::Deserialize;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("Hello <strong>World!!!</strong>") }),
-    );
+    let routes_all = Router::new().merge(routes_hello());
 
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
     println!(
@@ -14,7 +17,34 @@ async fn main() {
         listener.local_addr().unwrap()
     );
 
-    axum::serve(listener, routes_hello.into_make_service())
+    axum::serve(listener, routes_all.into_make_service())
         .await
         .unwrap();
+}
+
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+//  eg, `/hello?name=xyz`
+async fn handler_hello(params: Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World!!!");
+
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+
+//  eg, `/hello2/ggs`
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+
+    Html(format!("Hello <strong>{name}</strong>"))
 }
