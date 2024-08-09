cbef531491a6f8b962eb63bdef0f5c05edf04d47
deepso <deeps.o7o8.y@gmail.com>
2024-08-10 03:05:55 +0530

    add cookies

--- src/web/routes_login.rs
+++ src/web/routes_login.rs
@@ -1,14 +1,15 @@
 use axum::{routing::post, Json, Router};
 use serde::Deserialize;
 use serde_json::{json, Value};
+use tower_cookies::{Cookie, Cookies};
 
-use crate::{Error, Result};
+use crate::{web, Error, Result};
 
 pub fn routes() -> Router {
     Router::new().route("/api/login", post(api_login))
 }
 
-async fn api_login(payload: Json<LoginPayload>) -> Result<Json<Value>> {
+async fn api_login(cookies: Cookies, payload: Json<LoginPayload>) -> Result<Json<Value>> {
     println!("->> {:<12} - api_login", "HANDLER");
 
     // TODO: impl db and auth
@@ -17,7 +18,8 @@ async fn api_login(payload: Json<LoginPayload>) -> Result<Json<Value>> {
         return Err(Error::LoginFail);
     }
 
-    // TODO: Set cookie
+    // TODO: impl real token logic
+    cookies.add(Cookie::new(web::AUTH_TOKEN, "user-1.exp.sign"));
 
     let body = Json(json!({
       "result": {
