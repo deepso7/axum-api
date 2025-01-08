7782e597ba45f16bec59c6414cb607d9ffc4760b
deepso <deeps.o7o8.y@gmail.com>
2025-01-08 19:30:08 +0530

    add client errors

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,15 @@ use axum::{
     middleware,
     response::{Html, IntoResponse, Response},
     routing::{get, get_service},
-    Router,
+    Json, Router,
 };
 use model::ModelController;
 use serde::Deserialize;
+use serde_json::json;
 use tokio::net::TcpListener;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
 mod ctx;
 mod error;
@@ -54,9 +56,35 @@ async fn main() -> Result<()> {
 async fn main_response_mapper(res: Response) -> Response {
     println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
 
+    let uuid = Uuid::new_v4();
+
+    //  --  Get eventual response error
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    //  --  If client error, build a new response
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string()
+                }
+            });
+
+            println!("  ->> client_error_body: {client_error_body}");
+
+            // Build new response from client_error_body
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    //  --  TODO: Build and log the server log line
+    println!("  ->> server log line - {uuid} - Error: {service_error:?}");
+
     println!();
 
-    res
+    error_response.unwrap_or(res)
 }
 
 fn routes_static() -> Router {
